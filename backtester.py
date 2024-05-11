3efc178a2df773030b92be3de74e35b2b1f1b72d
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2024-05-10 23:00:29 -0700

    added a plot to a tk enviroment

--- backtester.py
+++ backtester.py
@@ -2,6 +2,14 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
 
+#code for inputing matplotlib inside of Tkinter app
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+
+#creating root window for tkiner
+root = tk.Tk()
+
 # Fake Data
 x = np.linspace(0,10,100)
 y1 = np.sin(x)
@@ -27,4 +35,10 @@ axs[2].set_title("MACD")
 #Top margin for graphics -gives space not so cramp
 plt.subplots_adjust(hspace=0.5)
 
-plt.show()
+#Creating tk canvas and inserting plots within ~ later used for collecting user inputs
+canvas = FigureCanvasTkAgg(fig,master=root)
+canvas.draw()
+canvas.get_tk_widget().pack()
+root.mainloop()
+root.destroy()
+#plt.show()
