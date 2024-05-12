921fdab708b3e328f51f6c551667055e17911d0e
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2024-05-11 22:42:42 -0700

    created shared x axis amoung graphs

--- backtester.py
+++ backtester.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.ticker import MultipleLocator
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
 import tkinter as tk
+from tkinter import messagebox
+
+import sys
 import numpy as np
 import pandas as pd
 
@@ -11,15 +14,18 @@ class Applicaton:
         def __init__(self, root):
                 self.root = root
                 self.root.title("My Tkinter App")
+                self.root.geometry("1200x700")
                 self.loadData()
                 self.fig = plt.figure(figsize=(8,6))
                 self.plotData()
+                self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         
         def loadData(self):
                 # Fake Data
                 self.df = pd.read_csv("BTC-USD.csv")
                 self.dates = self.df['Date']
                 self.closes = self.df['AdjClose']
+                self.volume = self.df['Volume']
                 self.x = np.linspace(0,10,100)
                 self.y1 = np.sin(self.x)
                 self.y2 = np.cos(self.x)
@@ -28,41 +34,61 @@ class Applicaton:
         def plotData(self):
                 #adds height to chart types, aka top chart is tallest
                 self.gs = gridspec.GridSpec(3,1,height_ratios=[5,1,1])
-                
-                self.axs = [self.fig.add_subplot(self.gs[0]),
-                        self.fig.add_subplot(self.gs[1]),
-                        self.fig.add_subplot(self.gs[2])]
 
-                        #plots
+                self.axs = [self.fig.add_subplot(self.gs[0])]
+                self.axs.append(self.fig.add_subplot(self.gs[1],sharex = self.axs[0]))
+                self.axs.append(self.fig.add_subplot(self.gs[2],sharex = self.axs[0]))
+
+                #plots
                 self.axs[0].plot(self.dates,self.closes)
-                self.axs[0].xaxis.set_tick_params(rotation=25)
-                self.axs[0].xaxis.set_major_locator(MultipleLocator(25))
+                self.axs[2].xaxis.set_tick_params(bottom = False)
 
-                self.axs[1].plot(self.x,self.y2)
+                self.axs[1].plot(self.dates,self.volume)
                 self.axs[1].set_title("Volume")
-                self.axs[2].plot(self.x,self.y3)
+
+                self.axs[2].plot(self.dates,self.volume)
                 self.axs[2].set_title("MACD")
+                self.axs[2].xaxis.set_tick_params(rotation=25)
+                self.axs[2].xaxis.set_major_locator(MultipleLocator(25))
+
+                self.axs[-1].set_xlabel("Dates")
+
+                for ax in self.axs:
+                        if ax != self.axs[-1]:
+                                ax.get_xaxis().set_visible(False)  # Hide x axis for top graphs
+
 
                 #Top margin for graphics -gives space not so cramp
-                plt.subplots_adjust(hspace=0.5)
+                plt.subplots_adjust(hspace=0.2)
 
                 # Create a label
-                self.label = tk.Label(self.root, text="Hello, Tkinter!")
-                self.label.pack()
+                # self.label = tk.Label(self.root, text="Hello, Tkinter!")
+                # self.label.pack()
 
                 #Creating tk canvas and inserting plots within ~ later used for collecting user inputs
                 self.canvas = FigureCanvasTkAgg(self.fig,master=self.root)
                 self.canvas.draw()
-                self.canvas.get_tk_widget().pack()
-                
-                # Create a button
+                self.canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+
+                # Add Matplotlib navigation toolbar
+                self.toolbar = NavigationToolbar2Tk(self.canvas, self.root)
+                self.toolbar.update()
+                self.toolbar.pack(side=tk.BOTTOM, fill=tk.X)  # Pack horizontally
+
+                #Create a button
                 self.button = tk.Button(root, text="Click me!", command=self.change_label)
                 self.button.pack()
 
         def change_label(self):
                 self.label.config(text="Button clicked!")
 
+        def on_closing(self):
+                if messagebox.askokcancel("Quit", "Are you sure you want to quit?"):
+                        self.root.destroy() # destroys current tkinter instance
+                        sys.exit()  # Exit the program
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = Applicaton(root)
     root.mainloop()
+
